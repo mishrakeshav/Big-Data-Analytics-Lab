6ba1f8ac632efb198013bba94117538ab04d8eba
mishrakeshav <keshav.sm@somaiya.edu>
2022-03-22 19:45:40 +0530

    update

--- Frequent-Itemset-Mining/Apriori.java
+++ Frequent-Itemset-Mining/Apriori.java
@@ -3,10 +3,20 @@ import java.util.*;
 import java.io.*;
 import java.util.Scanner;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
-import org.apache.hadoop.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Project {
+
+public class Apriori {
 	static int ntrans=0;
 
   public static class MapperClass 

843b392bb27dbb17f2086e65d3fe91a4e34c889c
mishrakeshav <keshav.sm@somaiya.edu>
2022-05-05 07:29:53 +0530

    update

---
+++ Viva/worcount-mapreduce/mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in sorted(words):
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print(f"{word}\t1")

8f7b9d1b42072c00fec5ea2cbf318c290bb66215
mishrakeshav <keshav.sm@somaiya.edu>
2022-05-05 10:26:10 +0530

    update

--- Viva/matmul/test_naive.sh
+++ Viva/matmul/test_naive.sh
@@ -2,4 +2,10 @@
 
 module load python/gnu/3.4.4
 
-cat data/matsmall.txt | python src/naive_map.py 2 3 | sort -n | python src/naive_reduce.py 5
+cat data/test_data.txt | python3 src/naive_map.py 2 3 | sort -n | python3 src/naive_reduce.py 5
+
+
+hadoop jar hadoop-streaming-3.2.3.jar \
+-file ./mapper.py    -mapper "python3 mapper.py" \
+-file ./reducer.py   -reducer "python3 reducer.py" \
+-input /wordcount/input.txt -output /wordcount/output22.txt
--- Viva/worcount-mapreduce/notes.md
+++ Viva/worcount-mapreduce/notes.md
@@ -1,5 +1,5 @@
 
 hadoop jar hadoop-streaming-3.2.3.jar \
--file ./mapper.py    -mapper "python3 mapper.py" \
--file ./reducer.py   -reducer "python3 reducer.py" \
--input /wordcount/input.txt -output /wordcount/output22.txt
+-file ./src/naive_map.py    -mapper "python3 naive_map.py 2 3" \
+-file ./src/naive_reduce.py   -reducer "python3 naive_reduce.py 5" \
+-input /matmul/test_data.txt -output /mat_mul/output
